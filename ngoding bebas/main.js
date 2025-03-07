565a58f14187da2c9577bf2ac4bb88d2ea14da51
RamziWafi-20 <ramziwafia@gmail.com>
2025-03-07 20:58:43 +0700

    Add files via upload

---
+++ ngoding bebas/main.js
@@ -0,0 +1,11 @@
+var uin = confirm("APAKAH INI REVA???")
+if(uin === true) {
+    alert("HAPPY BDAY REVA")
+    alert("AKU SANGAT BERSYUKUR")
+    alert("JANGAN LUPA MANDI")
+    alert("Trimakasih sudah ingin bersama ku")
+    alert("LOVEEE U PESEK")
+}
+else{
+    alert("ini bukan reva")
+}
